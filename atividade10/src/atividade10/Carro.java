136edf0af931b5436363b217d8f3af3abcfd2b2e
Bruna <carolinereinertbruna@gmail.com>
2022-10-03 16:57:39 -0300

    Atividade dia 10

---
+++ atividade10/src/atividade10/Carro.java
@@ -0,0 +1,15 @@
+package atividade10;
+
+public class Carro extends Veiculo {
+
+	public String placa;
+	
+	public void ligar() {
+		
+	}
+	
+	public void desligar() {
+		
+	}
+	
+}
---
+++ atividade10/src/atividade10/Veiculo.java
@@ -0,0 +1,11 @@
+package atividade10;
+
+public class Veiculo {
+
+	public String marca;
+	public String modelo;
+	
+	public void acelerar() {
+		
+	}
+}
